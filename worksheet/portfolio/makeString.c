9561ad8ea54d98510cfa1d805d21b95af358456d
Mark Walkley <116158358+cscmaw@users.noreply.github.com>
2026-02-12 21:56:38 +0000

    worksheet

---
+++ worksheet/portfolio/makeString.c
@@ -0,0 +1,19 @@
+
+/*
+Name:
+Student ID:
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main( int argc, char **argv ) {
+
+    // define appropriate data to hold your answer
+
+    // process the command-line data using appropriate string functions
+
+    printf("%s\n",buffer); // use only this single print statement in your submitted code
+
+    return 0;
+}
