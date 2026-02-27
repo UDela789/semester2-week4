e50d024f07f13ef731b7101be50360fab2ccc535
Delalorm Kunutsor <gmbp0709@leeds.ac.uk>
2026-02-27 15:36:19 +0000

    sdf

--- session-2/task2/string_ops.c
+++ session-2/task2/string_ops.c
@@ -14,10 +14,16 @@ int main( void ) {
     // use printf to verify the following tests
 
     // use 'strcmp' to compare 2 strings - print the return value
+    int cmp = strcmp(str1, str2);
+    printf("%d\n", cmp);
 
     // use 'strcat' to concatenate 2 strings - print the resulting string
+    strcat(str1,str2);
+    printf("%s\n", str1);
 
     // use 'strcpy' to copy a string into str3 - print string 3
+    strcpy(str3, str1);
+    printf("%s\n"str3);
 
     return 0;
 }
--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name:Delalorm Kunutsor
+Student ID: gmbp0709
 */
 
 #include <stdio.h>
@@ -10,8 +10,14 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[100];
 
     // process the command-line data using appropriate string functions
+    strcat(buffer, argv[1]);
+     for( int k=2; k<argc; ++k ) {
+        strcat(buffer, "-");
+        strcat(buffer, argv[k]);
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
